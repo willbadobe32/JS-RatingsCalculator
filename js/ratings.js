dcc6ed64a4ae3c72ccbfc324a5b1d367a57ccebe
willbadobe32 <willbadobe32@users.noreply.github.com>
2022-12-09 20:40:55 -0600

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -1,8 +1,8 @@
-collect_ratings() = {
+function collect_ratings() {
     const ratings = {
-        count: 0,
-        sum: 0,
-        average: 0
-    }
+        count: '0',
+        sum: '0',
+        average: '0'
+    };
     let rating = 0;
-};
+}
