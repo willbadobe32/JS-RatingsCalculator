f71dbb553645750845dfc11d2f2dd645d478a9c9
willbadobe32 <willbadobe32@users.noreply.github.com>
2022-12-11 02:18:01 -0600

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -1,7 +1,11 @@
 
 
 const collect_ratings = () => {
-    const ratings = { count: '0', sum: '0', average: '0'};
+    const ratings = {
+        count: 0,
+        sum: 0,
+        average: 0,
+      };
     let rating = 0;
 };
 //element variable is an array of ratings
