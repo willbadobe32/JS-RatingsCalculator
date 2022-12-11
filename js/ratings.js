525f577177be72f4fd8c8fe2a09117b869b2eefa
willbadobe32 <willbadobe32@users.noreply.github.com>
2022-12-11 02:59:33 -0600

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -4,7 +4,8 @@ const collect_ratings = () => {
 const ratings = { count: 0, sum: 0, average: 0 };
 
 let rating = 0;
-const elements = document.querySelectorAll('.ratings');
+const elements = document.querySelectorAll('.rating');
+
 elements.forEach( function (element) {});
 
 };
