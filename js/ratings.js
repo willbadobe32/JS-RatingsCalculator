6caa8d74d7eda7b03a225a0f594ddb2d9ad49b7d
willbadobe32 <willbadobe32@users.noreply.github.com>
2022-12-11 03:34:38 -0600

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -5,9 +5,10 @@ const ratings = { count: 0, sum: 0, average: 0 };
 
 let rating = 0;
 const elements = document.querySelectorAll('.rating');
-
-elements.forEach(element => {
+//Cycle through the elements array
+    elements.forEach(element => {
     rating = parseInt(element.id.replace('star', ''));
+    ratings.count += parseInt(element.value);
 });
 
 
