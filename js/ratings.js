07500008a905eb11057232adac121d012c75e96d
willbadobe32 <willbadobe32@users.noreply.github.com>
2022-12-11 23:09:14 -0600

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -9,9 +9,15 @@ const elements = document.querySelectorAll('.rating');
     elements.forEach(element => {
     rating = parseInt(element.id.replace('star', ''));
     ratings.count += parseInt(element.value);
+    ratings.sum += parseInt(element.value) * rating;
+    
 });
 
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count; 
+    }
 
+    return ratings;
 };
 
  
